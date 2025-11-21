24aa412cd28ee388f1313ed3fa5296b5b2191c23
sher304 <esherovshermat@gmail.com>
2025-11-21 16:34:22 +0100

    Empl constructor impl

--- src/main/java/org/example/Adapter/Employee.java
+++ src/main/java/org/example/Adapter/Employee.java
@@ -1,4 +1,17 @@
 package org.example.Adapter;
 
 public class Employee {
+
+    public int ID;
+    public String Name;
+    public String Designation;
+    public double Salary;
+
+    public Employee(int ID, String name, String designation, double salary) {
+        this.ID = ID;
+        Name = name;
+        Designation = designation;
+        Salary = salary;
+    }
 }
+
