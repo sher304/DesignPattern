84e7c8300abb91d493f1fff1f51093e96ec99361
sher304 <esherovshermat@gmail.com>
2025-11-21 16:35:40 +0100

    Billing System impl

--- src/main/java/org/example/Adapter/BillingSystem.java
+++ src/main/java/org/example/Adapter/BillingSystem.java
@@ -1,4 +1,12 @@
 package org.example.Adapter;
 
+import java.util.List;
+
 public class BillingSystem {
+
+    public void processSalary(List<Employee> employeeList) {
+        for(Employee employee: employeeList) {
+            System.out.println("Rs." + employee.Salary + " Salary Credited to " + employee.Name + " Account");
+        }
+    }
 }
